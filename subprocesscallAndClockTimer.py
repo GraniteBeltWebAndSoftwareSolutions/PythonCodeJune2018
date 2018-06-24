7a173aacb85edbcdd2f6696d75ea81c1b2265569
GraniteBeltWebAndSoftwareSolutions <40533161+GraniteBeltWebAndSoftwareSolutions@users.noreply.github.com>
2018-06-24 14:46:31 +1000

    version with warnings and more comments

--- subprocesscallAndClockTimer.py
+++ subprocesscallAndClockTimer.py
@@ -2,11 +2,14 @@
 # import subprocess
 import os
 import time
-
+# Control-c to quit Python
+# this command line works too 
 # os.system(r'C:\Users\Tim\Downloads\WinVICE-2.4-x64\WinVICE-2.4-x64\x64.exe')
 
 def procedure():
-    time.sleep(3.5)
+    time.sleep(4.5) # time must be at least a few seconds or the Emulator 
+                    # starts too quickly and makes the computeralmost crash
+    print "WARNING! Don't leave program to run too long - Control-c to quit Python "
     st = os.startfile(r'C:\Users\Tim\Downloads\WinVICE-2.4-x64\WinVICE-2.4-x64\x64.exe')
     
     
